ba854fe7351c9681eab2f9a63f1ef1c5026b4e30
Elevista <elevista@sunnyholic.com>
2018-12-17 05:24:27 +0900

    git-amend: author,email,message edit mode

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 #!/usr/bin/env node
 const moment = require('moment')
-const { MultiSelect, Select, Snippet } = require('enquirer')
+const { MultiSelect, Select, Snippet, Form } = require('enquirer')
 const { exec, makeEcho, colors: c } = require('./util')
 const format = 'ddd MMM DD HH:mm YYYY Z'
 const formatDispay = 'YYYY-MM-DD HH:mm'
 
-  // %H: commit hash
-  // %h: abbreviated commit hash
-  // %an: author name
-  // %ae: author email
-  // %ad: author date
-  // %cn: committer name
-  // %ce: committer email
-  // %cd: committer date
-  // %s: subject
-  // %n: newline
+// %H: commit hash
+// %h: abbreviated commit hash
+// %an: author name
+// %ae: author email
+// %ad: author date
+// %cn: committer name
+// %ce: committer email
+// %cd: committer date
+// %s: subject
+// %n: newline
+
+const setEnv = function ({ name, email, date }) {
+  return Object.assign(process.env, {
+    'GIT_AUTHOR_NAME': name,
+    'GIT_AUTHOR_EMAIL': email,
+    'GIT_AUTHOR_DATE': date,
+    'GIT_COMMITTER_NAME': name,
+    'GIT_COMMITTER_EMAIL': email,
+    'GIT_COMMITTER_DATE': date
+  })
+}
+const itemInfo = function ({ hs, date, name, subject, sequence }) {
+  return `${c.yellow`(${hs})`}${c.bold(date.format(formatDispay))} ${c.green.bold(name)} ${subject}${c.cyan`${sequence}`}`
+}
 
 ;(async function () {
   if (exec`git status -s -uno`.length) {
@@ -23,9 +37,9 @@ const formatDispay = 'YYYY-MM-DD HH:mm'
   }
   try { exec`git rebase --abort -q` } catch (e) {}
 
-  let stdout = exec`git log --format="${'%H%n%h%n%an%n%ae%n%ad%n%cn%n%ce%n%cd%n%s%n'}" -10`
+  const stdout = exec`git log --format="${'%H%n%h%n%an%n%ae%n%ad%n%cn%n%ce%n%cd%n%s%n'}" -10`
   const ref = {}
-  let commits = stdout.split('\n\n').map((x, idx) => {
+  const commits = stdout.split('\n\n').map((x, idx) => {
     const [hash, hs, name, email, date, cname, cemail, cdate, subject] = x.trim().split('\n')
     return {
       hash,
@@ -43,6 +57,18 @@ const formatDispay = 'YYYY-MM-DD HH:mm'
   }).filter(x => x.hash)
   commits.forEach(x => { ref[x.hash] = x })
 
+  const mode = {}
+  const modeName = await new Select({
+    name: 'mode',
+    message: 'Select mode',
+    choices: [
+      { name: 'info', message: 'Edit info', hint: `- author,email,message` },
+      { name: 'set', message: 'Set date', hint: `- change date individually` },
+      { name: 'adjust', message: 'Adjust date', hint: `- add duration to all selected` }
+    ]
+  }).run()
+  mode[modeName] = true
+
   const selectedCommits = (await new MultiSelect({
     name: 'value',
     message: 'Select commits to change',
@@ -52,42 +78,59 @@ const formatDispay = 'YYYY-MM-DD HH:mm'
     result (names) {
       return Object.values(this.map(names))
     },
-    choices: commits.map(({ hash, hs, name, date, subject }) => {
-      return {
-        name: `${c.yellow`(${hs})`}${c.bold(date.format(formatDispay))}`,
-        hint: `${c.green.bold(name)} ${subject}`,
-        value: hash
-      }
-    })
+    choices: commits.map(({ hash, hs, name, date, subject }) => ({
+      name: `${c.yellow`(${hs})`}${c.bold(date.format(formatDispay))}`,
+      hint: `${c.green.bold(name)} ${subject}`,
+      value: hash
+    }))
   }).run()).map(x => ref[x])
 
   commits.reverse()
   selectedCommits.reverse()
-
   selectedCommits.forEach(x => { x.rebase = 'edit' })
   while (commits[0] && (commits[0].rebase === 'pick')) commits.shift()
   const rebaseString = commits.map(({ hs, subject, rebase }) => `${rebase} ${hs} ${subject}`).join('\n')
   process.env['GIT_SEQUENCE_EDITOR'] = makeEcho(rebaseString) + '>'
 
-  const method = await new Select({
-    name: 'method',
-    message: 'Select moment manipulate method',
-    choices: ['Set individually', 'Adjust all']
-  }).run()
+  async function editInfo ({ subject, hs, name, email, date, cname, cemail, cdate }, sequence) {
+    const message = itemInfo({ hs, date, name, subject, sequence })
+    const to = await new Form({
+      name: 'commit',
+      message,
+      choices: [
+        { name: 'name', message: 'Name', initial: name },
+        { name: 'email', message: 'Email', initial: email },
+        { name: 'subject', message: 'Message', initial: subject }
+      ]
+    }).run()
+    const diff = { name: name !== to.name, email: email !== to.email, subject: subject !== to.subject }
+    to.date = date
+    q.push(() => {
+      c.println(message)
+      setEnv(to)
+      if (diff.name || diff.email) {
+        c.println`${name} <${email}> -> ${to.name} <${to.email}>`
+        exec`git commit --amend --no-edit --author="${to.name} <${to.email}>"`
+      }
+      if (diff.subject) {
+        c.println`${subject} -> ${to.subject}`
+        exec`git commit --amend --no-edit -m "${to.subject}"`
+      }
+      exec`git rebase --continue`
+    })
+  }
 
-  const individual = method === 'Set individually'
-  const fn = individual ? 'moment' : 'moment.duration'
-  const days = individual ? 'date' : 'days'
-  const template = `${fn}({
+  const q = []
+  async function askTime (message, date) {
+    const fn = mode.set ? 'moment' : 'moment.duration'
+    const days = mode.set ? 'date' : 'days'
+    const template = `${fn}({
     minutes: ${c.yellow`\${minutes}`},
     hours: ${c.yellow`\${hours}`},
     ${days}: ${c.yellow`\${${days}}`},
     months: ${c.yellow`\${months}`},
     years: ${c.yellow`\${years}`}
   })`
-
-  let timeUnit
-  async function askTime (message, date) {
     const values = date ? date.toObject() : {
       minutes: 0,
       hours: 0,
@@ -109,31 +152,25 @@ const formatDispay = 'YYYY-MM-DD HH:mm'
     }).run()
     return ret
   }
-
-  const q = []
-  for (let i = 0; i < selectedCommits.length; i++) {
-    const { subject, hs, name, email, date, cname, cemail } = selectedCommits[i]
-    const sequence = c.cyan`(${i + 1}/${selectedCommits.length})`
+  let timeUnit
+  async function changeTime ({ subject, hs, name, email, date, cname, cemail }, sequence) {
     const from = date.format(formatDispay)
-    if (individual) {
-      timeUnit = await askTime(`${c.yellow`(${hs})`}${c.bold(from)} ${c.green.bold(name)} ${subject}${sequence}`, date)
-    } else if (!timeUnit) timeUnit = await askTime('Duration to add')
-    const m = individual ? moment(timeUnit) : date.add(timeUnit)
+    if (mode.set) timeUnit = await askTime(itemInfo({ hs, date, name, subject, sequence }), date)
+    else if (!timeUnit) timeUnit = await askTime('Duration to add')
+    const m = mode.set ? moment(timeUnit) : date.add(timeUnit)
     const newDate = m.format(format)
     const to = m.format(formatDispay)
-    q.push(function () {
+    q.push(() => {
       c.println`${c.yellow`(${hs})`}${c.bold(subject)} ${c.bold.cyan(from)} -> ${c.bold.green(to)} `
-      Object.assign(process.env, {
-        'GIT_AUTHOR_NAME': name,
-        'GIT_AUTHOR_EMAIL': email,
-        'GIT_AUTHOR_DATE': newDate,
-        'GIT_COMMITTER_NAME': cname,
-        'GIT_COMMITTER_EMAIL': cemail,
-        'GIT_COMMITTER_DATE': newDate
-      })
-      exec`git commit --amend --date="${newDate}" --no-edit`
+      setEnv({ name, email, date: newDate })
+      exec`git commit --amend --no-edit --date="${newDate}"`
       exec`git rebase --continue`
     })
+  }
+  for (let i = 0; i < selectedCommits.length; i++) {
+    const sequence = `(${i + 1}/${selectedCommits.length})`
+    if (mode.info) await editInfo(selectedCommits[i], sequence)
+    else await changeTime(selectedCommits[i], sequence)
   }
   try { exec`git rebase -i ${commits[0].hash}` } catch (e) {}
   q.forEach(x => x())
--- util.js
+++ util.js
@@ -1,19 +1,17 @@
 const win32 = process.platform === 'win32'
 const { execSync } = require('child_process')
-const escapeRegex = win32 ? [/([%()^])/g, '^$1'] : [/(["\\])/g, '\\$1']
-const lineWrap = win32 ? '()' : '""'
+const escapeRegex = win32 ? [/([%()^])/g, '^$1'] : [/(["\\;])/g, '\\$1']
 const c = require('ansi-colors')
 const joinTpl = (str, exp) => str.map((x, i) => x + (exp[i] || '')).join('')
+const joinChar = win32 ? '\n' : ';'
 
 const escape = function (str, ...exp) {
-  let ret = str.map((x, i) => {
+  return str.map((x, i) => {
     let ret = (exp[i] || '').replace(...escapeRegex)
-    if (!win32) ret = /\n/.test(ret) ? lineWrap[0] + ret + lineWrap[1] : ret
     return x + ret
   }).join('')
-  return win32 && /\n/.test(ret) ? `(${ret})` : ret
 }
-const makeEcho = win32 ? str => escape`${str.split('\n').map(x => `echo ${x}`).join('\n')}` : str => escape`echo ${str}`
+const makeEcho = str => `(${str.split('\n').map(x => escape`echo ${x}`).join(joinChar)})`
 const exec = (...args) => execSync(escape(...args)).toString()
 
 const colors = {
